ac3e0362d2fc08c10f543e9f6fa5abe347443a31
Andreas Sommer <andreas.sommer87@googlemail.com>
2022-02-15 05:48:48 +0100

    Remove `bottle :disable` since it was deprecated without replacement

--- cryptopp.rb
+++ cryptopp.rb
@@ -5,9 +5,6 @@ class Cryptopp < Formula
   sha256 "8a4e4773a39b0c07d7cea1b8be7a3f7a9d126bd3ac9a9f072f82d3a53a474a87"
   license "BSD-3-Clause"
 
-  # https://cryptopp.com/wiki/Config.h#Options_and_Defines
-  bottle :disable, "Library and clients must be built on the same microarchitecture"
-
   def install
     system "make", "shared", "all", "CXX=#{ENV.cxx}"
     system "./cryptest.exe", "v"
